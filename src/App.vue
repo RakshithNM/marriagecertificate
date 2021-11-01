6677d1dcc8ca353c62cd4e74ed76321fe608f73f
Rakshith Bellare <rb@nuiteq.com>
2021-11-01 20:33:35 +0530

    Minimum viable product of the certificate with dynamic name address of the bride and groom

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,121 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header class="no-print" title="MARRIAGE CERTIFICATE" subText="SHREE PERNE MUCHILOT BHAGAVATHI KSHETHRA"/>
+    <Form 
+      class="no-print"
+      @name-change="updateNameGroom($event, 'groomName')" 
+      @address1-change="updateAddress1Groom($event, 'groomAddress1')" 
+      @address2-change="updateAddress2Groom($event, 'groomAddress2')"
+      @address3-change="updateAddress3Groom($event, 'groomAddress3')" />
+    <Form 
+      class="no-print"
+      @name-change="updateNameBride($event, 'brideName')" 
+      @address1-change="updateAddress1Bride($event, 'brideAddress1')" 
+      @address2-change="updateAddress2Bride($event, 'brideAddress2')"
+      @address3-change="updateAddress3Bride($event, 'brideAddress3')" />
+
+    <div class="no-print">
+      <label for="date">DATE</label>
+      <input
+        id="date" name="date" type="date"
+        v-model="date" />
+
+      <label for="registernumber">REGISTER NUMBER</label>
+      <input 
+        id="registernumber" name="registernumber" type="text"
+        v-model="registerNumber" />
+    </div>
+
+    <div class="container">
+      <div class="centered">
+        <div>
+          <h1>{{ nameGroom }}</h1>
+          <h3>{{ address1Groom }}</h3>
+          <h3>{{ address2Groom }}</h3>
+          <h3>{{ address3Groom }}</h3>
+        </div>
+        <h3>AND</h3>
+        <div>
+          <h1>{{ nameBride }}</h1>
+          <h3>{{ address1Bride }}</h3>
+          <h3>{{ address2Bride }}</h3>
+          <h3>{{ address3Bride }}</h3>
+        </div>
+      </div>
+      <h3 class="date">{{ date }}</h3>
+      <h3 class="registerNumber">{{ registerNumber }}</h3>
+      <img src="./assets/base.jpg" alt="certificate base image" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Form
+  },
+  data() {
+    return {
+      nameGroom: "",
+      address1Groom: "",
+      address2Groom: "",
+      address3Groom: "",
+      nameBride: "",
+      address1Bride: "",
+      address2Bride: "",
+      address3Bride: "",
+      date: "",
+      registerNumber: ""
+    }
+  },
+  methods: {
+    updateNameGroom(inName, inType) {
+      console.log(inName, inType);
+      if(inType === 'groomName') {
+        this.nameGroom = inName;
+      }
+    },
+    updateAddress1Groom(inAddress1, inType) {
+      if(inType === 'groomAddress1') {
+        this.address1Groom = inAddress1;
+      }
+    },
+    updateAddress2Groom(inAddress2, inType) {
+      if(inType === 'groomAddress2') {
+        this.address2Groom = inAddress2;
+      }
+    },
+    updateAddress3Groom(inAddress3, inType) {
+      if(inType === 'groomAddress3') {
+        this.address3Groom = inAddress3;
+      }
+    },
+    updateNameBride(inName, inType) {
+      console.log(inName, inType);
+      if(inType === 'brideName') {
+        this.nameBride = inName;
+      }
+    },
+    updateAddress1Bride(inAddress1, inType) {
+      if(inType === 'brideAddress1') {
+        this.address1Bride = inAddress1;
+      }
+    },
+    updateAddress2Bride(inAddress2, inType) {
+      if(inType === 'brideAddress2') {
+        this.address2Bride = inAddress2;
+      }
+    },
+    updateAddress3Bride(inAddress3, inType) {
+      if(inType === 'brideAddress3') {
+        this.address3Bride = inAddress3;
+      }
+    },
   }
 }
 </script>
@@ -24,5 +128,59 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  position: relative;
+  text-align: center;
+  color: black;
+  width: 21cm;
+  height: 29.7cm;
+}
+
+.centered {
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.centered > div > h3 {
+  margin: 0;
+}
+
+.centered > div > h1 {
+  margin: 10px;
+}
+
+.date {
+  position: absolute;
+  top: 64%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.registerNumber {
+  position: absolute;
+  top: 69%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+@page {
+  margin: 0;
+}
+
+@media print {
+  html, body {
+    height: 100%;
+    margin: 0 !important;
+    padding: 0 !important;
+    overflow: hidden;
+  }
+
+  .no-print, .no-print * {
+    display: none !important;
+  }
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,47 @@
+<template>
+  <form>
+    <label for="name">NAME</label>
+    <input
+      id="name" name="name" type="text"
+      v-model="name" @input="$emit('name-change', $event.target.value)" />
+
+    <div>
+      <h5>Address</h5>
+      <label for="address1">Address line 1</label>
+      <input
+        id="address1" name="address1" type="text"
+        v-model="address1" @input="$emit('address1-change', $event.target.value)" />
+
+      <label for="address2">Address line 2</label>
+      <input
+        id="address2" name="address2" type="text"
+        v-model="address2" @input="$emit('address2-change', $event.target.value)" />
+
+      <label for="address3">Address line 3</label>
+      <input
+        id="address3" name="address3" type="text"
+        v-model="address3" @input="$emit('address3-change', $event.target.value)" />
+    </div>
+
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data() {
+    return {
+      name: "",
+      address1: "",
+      address2: "",
+      address3: "",
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+</style>
