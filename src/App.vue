b013adc19c419dc2f83e885717a30c3bbe20f447
Rakshith Bellare <rb@nuiteq.com>
2021-11-11 17:58:46 +0530

    Added offline support

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,26 @@
       </div>
     </form>
 
+    <div class="update-dialog" v-if="showUpgradeUI">
+      <div class="update-dialog__content">
+        A new version is found. Refresh to load it?
+      </div>
+      <div class="update-dialog__actions">
+        <button
+          class="update-dialog__button update-dialog__button--confirm"
+          @click="update"
+        >
+          Update
+        </button>
+        <button
+          class="update-dialog__button update-dialog__button--cancel"
+          @click="prompt = false"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+
     <div class="containerd">
       <div class="centered">
         <div>
@@ -76,10 +96,22 @@ export default {
       address2Bride: "",
       address3Bride: "",
       date: "",
-      registerNumber: ""
+      registerNumber: "",
+      showUpgradeUI: false
+    }
+  },
+  created() {
+    if (this.$workbox) {
+      this.$workbox.addEventListener("waiting", () => {
+        this.showUpgradeUI = true;
+      });
     }
   },
   methods: {
+    async accept() {
+      this.showUpgradeUI = false
+      await this.$workbox.messageSW({ type: "SKIP_WAITING" });
+    },
     updateNameGroom(inName, inType) {
       if(inType === 'groomName') {
         this.nameGroom = inName;
@@ -215,5 +247,29 @@ input {
   .no-print, .no-print * {
     display: none !important;
   }
+}
+
+.update-dialog {
+  position: fixed;
+  left: 50%;
+  bottom: 64px;
+  transform: translateX(-50%);
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  padding: 12px;
+  max-width: 576px;
+  color: white;
+  background-color: #2c3e50;
+  text-align: left;
+}
+.update-dialog__actions {
+  display: flex;
+  margin-top: 8px;
+}
+.update-dialog__button {
+  margin-right: 8px;
+}
+.update-dialog__button--confirm {
+  margin-left: auto;
 }
 </style>
