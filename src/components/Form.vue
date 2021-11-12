cb6d912982e33db349c9c99afe277b54418b4d51
Rakshith Bellare <rb@nuiteq.com>
2021-11-12 20:22:30 +0530

    Updated placeholders

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,21 +6,21 @@
       :id="type === 'groom' ? 'groom-name' : 'bride-name'" name="name" type="text"
       v-model="name" @input="$emit('name-change', $event.target.value)" />
 
-    <label for="address1">FATHER AND MOTHER</label>
+    <label for="address1">PARENTS' DETAILS</label>
     <input
-      class="form-control" :placeholder="type === 'groom' ? 'S/O FATHER\'S NAME AND MOTHER\'S NAME' : 'D/O FATHER\'S NAME AND MOTHER\'S NAME'"
+      class="form-control" placeholder="PARENTS' DETAILS"
       :id="type === 'groom' ? 'groom-address1' : 'bride-address1'" name="address1" type="text"
       v-model="address1" @input="$emit('address1-change', $event.target.value)" />
 
-    <label for="address2">ADDRESS LINE 2</label>
+    <label for="address2">ADDRESS LINE 1</label>
     <input
-      class="form-control" placeholder="ADDRESS LINE 2"
+      class="form-control" placeholder="HOUSE NAME, PLACE, PANCHAYATH"
       :id="type === 'groom' ? 'groom-address2' : 'bride-address2'" name="address2" type="text"
       v-model="address2" @input="$emit('address2-change', $event.target.value)" />
 
-    <labeL FOR="ADDRESS3">ADDRESS LINE 3</label>
+    <labeL FOR="ADDRESS3">ADDRESS LINE 2</label>
     <input
-      class="form-control" placeholder="ADDRESS LINE 3"
+      class="form-control" placeholder="TALUK, DISTRICT, PINCODE"
       :id="type === 'groom' ? 'groom-address3' : 'bride-address3'" name="address3" type="text"
       v-model="address3" @input="$emit('address3-change', $event.target.value)" />
   </form>
